ab402ad322aa5be46093fcab54dd37d4f5db9eea
Dave Finnegan <dfinnegan@sonatype.com>
2012-05-30 12:08:31 -0400

    Initial commit

---
+++ src/main/java/org/sonatype/training/babble_core/App.java
@@ -0,0 +1,13 @@
+package org.sonatype.training.babble_core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
